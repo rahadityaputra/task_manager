2c9b2f23357f73682848eea282f2a7e587cc789a
rahadityaputra <rahadit.a.p123@gmail.com>
2024-05-15 12:18:28 +0700

    update

--- main.cpp
+++ main.cpp
@@ -4,14 +4,22 @@
 #include <string.h>
 #include <vector>
 #include <cstring>
+#include <fstream>
 
-// struct untuk menampung data user dari mydql
-//struct USER {
-//	char id[255];
-//	char username[50];
-//	char password[225];
-//	char created_at[50];
-//};
+#define MAX_USERNAME_LENGTH 50
+#define MAX_PASSWORD_LENGTH 255
+
+
+// TODO
+
+// MEMBUAT FUNCTION UNTUK MEMVALIDASI USERNAME DAN PASSWORD SAAT USER DAFTAR
+// MEMBUAT MENU UTAMA
+// MERAPIKAN ERD DATABASE YANG TELAH DIBUAT
+// MENGHAPUS SEMUA DATA YANG ADA DI DATABASE
+// MEMBUAT FUNCTION SAY HELLO
+// MERAPIKAN FORM LOGIN DENGAN LIBRARY IOMANIP
+
+
 
 struct USER {
 	std::string id;
@@ -19,6 +27,9 @@ struct USER {
 	std::string password;
 	std::string created_at;
 };
+
+std::ofstream cacheWrite;
+std::ifstream cacheRead;
 
 MYSQL* conn;
 MYSQL_RES* result;
@@ -32,6 +43,35 @@ char password[] = "";
 char database[] = "task_manager_db";
 int port = 3306;
 
+void sayHello(std::string name) {
+	std::cout << "Hello " << name << std::endl;
+}
+
+
+bool checkCache() {
+	std::string isCache;
+	cacheRead.open("cache.txt");
+	std::getline(cacheRead, isCache);
+	cacheRead.close();
+	if (isCache == "true") {
+		return true;
+	}
+	return false;
+}
+
+void createCache(char username[50], char password[500]) {
+	cacheWrite.open("cache.txt");
+	cacheWrite << "true";
+	cacheWrite << "\n" << username;
+	cacheWrite << "\n" << password;
+	cacheWrite.close();
+}
+
+void deleteCache() {
+	cacheWrite.open("cache.txt");
+	cacheWrite << "false";
+	cacheWrite.close();
+}
 
 
 
@@ -47,23 +87,53 @@ void getUserData() {
 	const char *query = "SELECT * FROM user";
 	if(!(mysql_query(conn,  query))) {
 		result = mysql_use_result(conn);
-	
 		while (row = mysql_fetch_row(result)) {
 			std::string column0 = row[0];
         	std::string column1 = row[1];
         	std::string column2 = row[2];
         	std::string column3 = row[3];
-			//std::cout << row[0] << " " << row[1] << " " << row[2] << " " << row[3] << std::endl;
 			userData.push_back({column0, column1, column2, column3});
 		}
-			int count = mysql_num_rows(result);
-		std::cout << "jumlah data = " << count;
 	} else {
 		std::cout << mysql_error(conn) ;
 	}
 	
 	mysql_free_result(result);
 }
+
+char* getUsernameDataInCacheFile() {
+	char* username;
+	cacheRead.open("cache.txt");
+	cacheRead >> username;
+	cacheRead >> username;
+	cacheRead.close();
+	return username;
+}
+
+
+std::string getNameDataByUsername(char username[50]) {
+	char query[500];
+	char escaped_username[100];
+	mysql_real_escape_string(conn, escaped_username, username, strlen(username));
+	sprintf(query,  "SELECT * FROM user WHERE username = '%s'", escaped_username);
+	if(!(mysql_query(conn,  query))) {
+		std::string name;
+		result = mysql_use_result(conn);
+		while (row = mysql_fetch_row(result)) {
+        	 name = row[3];
+		
+		}
+		
+	return name;
+	} else {
+		std::cout << mysql_error(conn) ;
+	}
+	
+	mysql_free_result(result);
+	
+}
+
+
 
 void displayData() {
 	for (int i = 0; i < userData.size(); i++) {
@@ -74,18 +144,23 @@ void displayData() {
   }
 }
 
-void addDataUser(const char* username, const char* password) {
+
+bool addDataUser(const char* name, const char* username, const char* password) {
     char query[500];
-    char escaped_username[100], escaped_password[450];
+    char escaped_username[100], escaped_password[450], escaped_name[500];
     mysql_real_escape_string(conn, escaped_username, username, strlen(username));
     mysql_real_escape_string(conn, escaped_password, password, strlen(password));
-    sprintf(query, "INSERT INTO user VALUES (NULL, '%s', '%s', current_timestamp())", escaped_username, escaped_password);
+	mysql_real_escape_string(conn, escaped_name, name, strlen(name));
+    sprintf(query, "INSERT INTO user VALUES (NULL, '%s', '%s', '%s', current_timestamp())", escaped_username, escaped_password, escaped_name);
     if (mysql_query(conn, query) == 0) {
         std::cout << "Data berhasil ditambahkan!" << std::endl;
+        return true;
     } else {
         std::cerr << "Gagal menambahkan data! Error: " << mysql_error(conn) << std::endl;
+        return false;
     }
 }
+
 
 bool  checkLogin(std::string username, std::string password) {
 	for (int i = 0; i < userData.size(); i++) {
@@ -98,7 +173,21 @@ bool  checkLogin(std::string username, std::string password) {
   return false;
 }
 
+void menu(/*char username[50]*/) {
+	std::cout << "ini menu";
+	//sayHello(getNameDataByUsername(username));
+	int choise;
+	std::cout << "selamat datang di menu utama" << std::endl;
+	std::cout << "0. log out" << std::endl;
+	std::cout << "choise : ";
+	std::cin >> choise;
+	if(choise == 0) {
+		deleteCache();
+	}
+}
+
 void logIn() {
+	system("cls");
 	char username[50];
 	char password[225];
 	std::cout << "======= Log In =======" << std::endl;
@@ -109,18 +198,22 @@ void logIn() {
 	getUserData();
 	if(checkLogin(username, password)) {
 		std::cout << "masuk menu" << std::endl;
+		createCache(username, password);
+		menu();
 	} else {
 		logIn();
+		
 	}
-	
-	
 }
 
 void signIn() {
-	char username[50];
-	char password1[225];
+	char name[500];
+	char username[MAX_USERNAME_LENGTH];
+	char password1[MAX_PASSWORD_LENGTH];
 	char password2[225];
 	std::cout << "======= Sign In =======" << std::endl;
+	std::cout << "Name : ";
+	std::cin >> name;
 	std::cout << "Username : ";
 	std::cin >> username;
 	std::cout << "Password : ";
@@ -129,61 +222,49 @@ void signIn() {
 	std::cin >> password2;
 
 	if (strcmp(password1, password2) == 0) {
-		addDataUser(username, password1);
-		
+		if(addDataUser(name, username, password1)) {
+			createCache(username, password1);
+			menu();	
+		}
 	} else {
 		std::cout << "pw 1 dan pw 2 beda" << std::endl;
 	}
 }
 
+
 int main() {
-	
 	getMysqlConnection();
+	
 	int choise;
 	if (conn) {
-		std::cout << "Welcome to Task Manager" << std::endl;
-		std::cout << "1. Log in\n2. Sign in" << std::endl;
-		std::cout << "Choise = " ; 
-		std::cin >> choise;
-		
-		switch(choise) {
-			case 1:
-				logIn();
-				break;
-			case 2:
-				signIn();
-				break;
-			case 3:
-				std::cout << "data data" << std::endl;		
-			default:
-				break;
+		if(checkCache()) {
+			menu();
+		} else {
+			std::cout << "Welcome to Task Manager" << std::endl;
+			std::cout << "1. Log in\n2. Sign in" << std::endl;
+			std::cout << "Choise = " ; 
+			std::cin >> choise;
+			
+			switch(choise) {
+				case 1:
+					logIn();
+					break;
+				case 2:
+					signIn();
+					break;
+				case 3:
+					std::cout << "data data" << std::endl;		
+				default:
+					break;
+			}
 		}
+		
+	
+		
 	} else {
 		std::cout << "Koneksi Database Eror" << std::endl;
 	}
 	
-	
-	
-	
-	
-	
-	// membuat form login
-	// membuat form sign in
-		//-saat sign in user diminta untuk memasukkan password 2 kali
-	
-	//std::cout << koneksi ;
-	
-//		if((!mysql_query(conn, "SELECT * FROM students"))) { 
-//						result = mysql_use_result(conn);
-//						while(row = mysql_fetch_row(result)) {
-//							std::cout <<  "ID = "<< row[0] << std::endl;
-//							std::cout <<  "Email = "<< row[1] << std::endl;
-//							std::cout <<  "Password = "<< row[2] << std::endl;
-//							std::cout <<  "Name = "<< row[3] << std::endl;
-//							std::cout <<  "NIM = "<< row[4] << std::endl;
-//							std::cout << std::endl;
-//						}
-//				}
 	
 	return 0;
 }
